72e878f57935db1c226c598d4f3072b13d958ef7
niraj1412 <14nirajkumar@gmail.com>
2024-02-14 12:22:54 +0530

    Committing changes before updating

---
+++ src/app/carousel-holder/carousel-holder.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarouselHolderComponent } from './carousel-holder.component';
+
+@NgModule({
+  declarations: [CarouselHolderComponent],
+  imports: [CommonModule],
+  exports: [CarouselHolderComponent]
+})
+export class CarouselHolderModule { }
